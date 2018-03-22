417a7ca001c405a2501e514ba68b940640d2de5a
Phan Thanh Thư <thanhthuk59@gmail.com>
2018-03-22 14:50:50 +0000

    add name and age

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180322131608) do
+ActiveRecord::Schema.define(version: 20180322144015) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -33,9 +33,12 @@ ActiveRecord::Schema.define(version: 20180322131608) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.integer  "age"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["name"], name: "index_users_on_name", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
